7a8a0466461b8137f9426afbb8b9490244f4394f
chaos-warzh <haidedeyu@163.com>
2025-01-14 14:23:51 +0800

    exercise13

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -15,9 +15,9 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
 
 // TODO: Fix the compiler error by changing the signature and body of the
 // `main` function.
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
-    let pretend_user_input = "8";
+    let pretend_user_input: &str = "8";
 
     // Don't change this line.
     let cost = total_cost(pretend_user_input)?;
@@ -28,4 +28,5 @@ fn main() {
         tokens -= cost;
         println!("You now have {tokens} tokens.");
     }
+    Ok(())
 }
